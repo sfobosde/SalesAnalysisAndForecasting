ec5bd4506579501478908eebff08a0de57f02283
Grigory Vasilev <grishavasilev62@gmail.com>
2022-05-24 18:49:44 +0300

    Add interface and abstract class

---
+++ SalesAnalysisAndForecastingBlankSolution/DataBaseLayerLib/DBAbstractFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DataBaseLayerLib
+{
+	/// <summary>
+	/// Connections layer.
+	/// </summary>
+	abstract class DBAbstractFactory : IDBFactory
+	{
+		#region Fields
+
+		/// <summary>
+		/// Connection data string from configuration.
+		/// </summary>
+		private string connectionString;
+		#endregion
+
+		#region
+		#endregion
+
+		#region
+		#endregion
+
+		#region Methods
+
+		public void GetConnectionString()
+		{
+			
+		}
+		#endregion
+	}
+}
