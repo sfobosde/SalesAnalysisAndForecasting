71205a1da6acacd66839d4bf1ee6bbae09a65fa5
Grigory Vasilev <grishavasilev62@gmail.com>
2022-06-04 14:55:06 +0300

    add user entity properties

--- SalesAnalysisAndForecastingBlankSolution/DataBaseLayerLib/Entities/User Entities/User.cs
+++ SalesAnalysisAndForecastingBlankSolution/DataBaseLayerLib/Entities/User Entities/User.cs
@@ -7,5 +7,25 @@ namespace DataBaseLayerLib.Entities.User_Entities
 	/// </summary>
 	class User
 	{
+		/// <summary>
+		/// User Id.
+		/// </summary>
+		public Guid UserId { get; set; }
+
+		/// <summary>
+		/// User login (domain).
+		/// </summary>
+		public string UserLogin { get; set; }
+
+		/// <summary>
+		/// User system name.
+		/// </summary>
+		public string UserName { get; set; }
+
+		/// <summary>
+		/// User password.
+		/// Change type.
+		/// </summary>
+		public string UserPassword { get; set; }
 	}
 }
