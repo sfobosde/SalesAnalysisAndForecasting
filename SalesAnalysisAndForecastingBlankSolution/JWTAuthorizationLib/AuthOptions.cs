27aec381c3644a6caddd5e0b3ab82cd9cae0029d
Grigory Vasilev <grishavasilev62@gmail.com>
2022-06-06 17:23:44 +0300

    Add AuthOptions class

---
+++ SalesAnalysisAndForecastingBlankSolution/JWTAuthorizationLib/AuthOptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTAuthorizationLib
+{
+    /// <summary>
+    /// Authorization options.
+    /// </summary>
+	class AuthOptions
+	{
+        /// <summary>
+        /// Token publisher.
+        /// </summary>
+        public const string ISSUER = "MyAuthServer";
+
+        /// <summary>
+        /// Token handler.
+        /// </summary>
+        public const string AUDIENCE = "MyAuthClient";
+
+        /// <summary>
+        /// Encryption key.
+        /// </summary>
+        const string KEY = "mysupersecret_secretkey!123";
+
+        /// <summary>
+        /// Token life time.
+        /// This is default time = 1 minute.
+        /// </summary>
+        public const int LIFETIME = 1;
+
+        /// <summary>
+        /// Generate security key.
+        /// </summary>
+        /// <returns></returns>
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
