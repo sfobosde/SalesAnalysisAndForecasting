073d99fcf4af089b6222684ae383d7bb40389202
Grigory Vasilev <grishavasilev62@gmail.com>
2022-06-12 02:09:10 +0300

    Write step instructions

--- SalesAnalysisAndForecastingBlankSolution/CalculationLib/CalculationModel.cs
+++ SalesAnalysisAndForecastingBlankSolution/CalculationLib/CalculationModel.cs
@@ -1,17 +1,23 @@
 ﻿using CalculationLib.Interfaces;
+using DataBaseLayerLib.Entities.AAFS_Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CalculationLib
 {
 	class CalculationModel : ICalculationModel
 	{
-		public void CalculateWeeklyDependecys()
+		public void CalculateWeeklyDependecys(List<ProductSales> productSales)
 		{
-			throw new NotImplementedException();
+			/// Find action free periods.
+			/// Divide on week periods.
+			/// Find holydays in periods (Irvin criterium (IC)).
+			/// Check by every of 7 week day dynamic:
+			/// Calculate weekly diferents;
+			/// Check by IC.
+			/// Mark diferents as weather infulence;
+			/// Find AVG.
+			/// Send to save in db.
 		}
 	}
 }
