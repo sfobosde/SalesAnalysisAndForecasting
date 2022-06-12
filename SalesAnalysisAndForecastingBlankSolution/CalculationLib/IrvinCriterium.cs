5f7b325b45292ab84d808c222c062fa63d155200
Grigory Vasilev <grishavasilev62@gmail.com>
2022-06-13 00:46:32 +0300

    add CommonStatisticCalculations

--- SalesAnalysisAndForecastingBlankSolution/CalculationLib/IrvinCriterium.cs
+++ SalesAnalysisAndForecastingBlankSolution/CalculationLib/IrvinCriterium.cs
@@ -1,32 +1,17 @@
 ﻿using CalculationLib.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CalculationLib
 {
 	/// <summary>
 	/// Calculate irvin cruterium.
 	/// </summary>
-	class IrvinCriterium : IIrvinCriterium
+	class IrvinCriterium : CommonStatisticCalculations, IIrvinCriterium
 	{
 		/// <summary>
-		/// Elements array;
+		/// Get abromal value index.
 		/// </summary>
-		private double[] valuesArray;
-
-		/// <summary>
-		/// Mathematics expectation (AVG value);
-		/// </summary>
-		private double mathExpectation;
-
-		/// <summary>
-		/// Dispersium.
-		/// </summary>
-		private double arrayVariance;
-
+		/// <returns></returns>
 		public int GetAbromalValueIndex()
 		{
 			double[] irvinCriterium = new double[valuesArray.Length];
@@ -36,7 +21,7 @@ namespace CalculationLib
 				valueIndex++)
 			{
 				irvinCriterium[valueIndex] = Math.Abs((
-					valuesArray[valueIndex] - valuesArray[valueIndex+1]) 
+					valuesArray[valueIndex] - valuesArray[valueIndex + 1]) 
 					/ arrayVariance);
 			}
 
@@ -52,45 +37,8 @@ namespace CalculationLib
 
 			return -1;
 		}
-
-		/// <summary>
-		/// Calculate mathematic expectation.
-		/// </summary>
-		private void CalculateMathExpectation()
+		public IrvinCriterium(double[] valuesArray) : base(valuesArray)
 		{
-			double valuesSum = 0;
-
-			for (int valueIndex = 0;
-				valueIndex < valuesArray.Length;
-				valueIndex++)
-			{
-				valuesSum += valuesArray[valueIndex];
-			}
-
-			mathExpectation = valuesSum / valuesArray.Length;
 		}
-
-		/// <summary>
-		/// Calculate dispersium.
-		/// </summary>
-		private void CalculateVariance()
-		{
-			double devitation = 0;
-
-			for (int valueIndex = 0;
-				valueIndex < valuesArray.Length;
-				valueIndex++)
-			{
-				devitation += Math.Pow(mathExpectation - valuesArray[valueIndex], 2);
-			}
-
-			arrayVariance = Math.Sqrt(devitation / valuesArray.Length - 1);
-		}
-
-		public IrvinCriterium(double[] valuesArray)
-		{
-			this.valuesArray = valuesArray;
-		}
-
 	}
 }
